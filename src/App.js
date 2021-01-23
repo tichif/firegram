01446ea35c9128ceb044c00fa1eeaf7f4f8a34ad
Dalzon Charles-Hébert <charlby5@gmail.com>
2021-01-22 23:59:44 -0600

    Create an UploadForm component, and an useStorage hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+
 import Title from './comps/Title';
+import UploadForm from './comps/UploadForm';
 
 function App() {
   return (
-    <div className="App">
-      <Title/>
+    <div className='App'>
+      <Title />
+      <UploadForm />
     </div>
   );
 }
